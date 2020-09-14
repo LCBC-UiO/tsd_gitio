3f66350c8dcf9dbecfb95b97f9394cffc2861be2
Florian Krull <flo.krull@gmail.com>
2020-09-15 00:03:03 +0200

    pull on fetch

--- lib/outside_fetch.sh
+++ lib/outside_fetch.sh
@@ -21,6 +21,7 @@ while read tsd_repos github_repos; do
     cd ${gdir}
     git remote set-url origin_github ${github_repos} 2>/dev/null || git remote add origin_github ${github_repos}
     git remote set-url origin_tsd ${tsd_repos} 2>/dev/null || git remote add origin_tsd ${tsd_repos}
+    git pull ${github_repos} master
   )
 done < <( cat ${BASEDIR}/cfg_repos.txt | grep -v "^#")
 
