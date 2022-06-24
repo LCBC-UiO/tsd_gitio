88a7a866c1a67b1accd3bfeaaf0f59be3fa5a8b3
Athanasia Monika Mowinckel <Athanasiamo@users.noreply.github.com>
2022-06-24 12:58:52 +0200

    fetch default branch, not necessarily master

--- lib/outside_fetch.sh
+++ lib/outside_fetch.sh
@@ -21,7 +21,7 @@ while read tsd_repos github_repos; do
     cd ${gdir}
     git remote set-url origin_github ${github_repos} 2>/dev/null || git remote add origin_github ${github_repos}
     git remote set-url origin_tsd ${tsd_repos} 2>/dev/null || git remote add origin_tsd ${tsd_repos}
-    git pull ${github_repos} master
+    git pull ${github_repos} $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
   )
 done < <( cat ${BASEDIR}/cfg_repos.txt | grep -v "^#" | grep ".")
 
